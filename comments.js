d10e9d928807fa83b4f2a9d7b1b6112ae966709a
Sai kumar Satapathy <66914177+kumar-sai-21@users.noreply.github.com>
2023-07-21 19:51:44 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,14 +1,65 @@
-// Create a web server for comments
-server.get('/comments', function(req, res) {
-    res.send(comments);
-    }  
-);
+// Create web server application for comments
 
+// Import modules
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
 
-// Path: comments.js
-// Create a web server for comments
-server.post('/comments', function(req, res) {
-    var comment = req.body;
-    comments.push(comment);
-    res.send(comment);
-    }
+// Set up web server
+app.set('port', process.env.PORT || 3000);
+
+// Set up body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Set up static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set up routes
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+app.get('/getComments', function(req, res) {
+    // Read comments.json
+    fs.readFile('comments.json', 'utf-8', function(err, data) {
+        if (err) {
+            throw err;
+        }
+
+        // Send comments as JSON
+        res.json(JSON.parse(data));
+    });
+});
+
+app.post('/addComment', function(req, res) {
+    // Read comments.json
+    fs.readFile('comments.json', 'utf-8', function(err, data) {
+        if (err) {
+            throw err;
+        }
+
+        // Parse comments.json
+        const comments = JSON.parse(data);
+
+        // Add comment to comments.json
+        comments.push(req.body.comment);
+
+        // Write comments to comments.json
+        fs.writeFile('comments.json', JSON.stringify(comments), function(err) {
+            if (err) {
+                throw err;
+            }
+
+            // Send comments as JSON
+            res.json(comments);
+        });
+    });
+});
+
+// Start web server
+app.listen(app.get('port'), function() {
+    console.log('Web server started on port ' + app.get('port'));
+});
